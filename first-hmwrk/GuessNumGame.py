abff12e1c16273becb1df191116b5f054193e691
Nawel Bellil <153316848+Nawel-Bellil@users.noreply.github.com>
2024-03-05 02:37:03 +0100

    completed

--- first-hmwrk/GuessNumGame.py
+++ first-hmwrk/GuessNumGame.py
@@ -1,59 +1,28 @@
-
-# ---------------------------------------------------------------------
-def new_game():
-
-    guesses = []
-    correct_guesses = 0
-    question_num = 1
-    answer = None
-
-    for key in questions:
-        print("---------------------------------")
-        print(key)
-        for option in answers[question_num-1]:
-           print(option)
-        answer = input("Enter the answer (A, B, C) : ").upper()
-        guesses.append(answer)
-
-        question_num+=1
-        correct_guesses += check_answer(questions.get(key),answer)
-    display_score(correct_guesses,guesses)
-    
-# ---------------------------------------------------------------------
-def check_answer(answer, guess):
-    if answer == guess:
-        print("CORRECT!")
-        return 1
-    else:
-        print("WRONG!")
-        return 0
-# ---------------------------------------------------------------------
-def play_again():
-    pass
-# ---------------------------------------------------------------------
-def display_score(correct_guesses,guesses):
-    print("---------------------------------------------------------------------")
-    print("RESULTS")
-    print("---------------------------------------------------------------------")
-    print("Answers: ", end="")
-    for i in questions:
-        print(questions.get(i),end="")
-    print("Guesses: ", end="")
-    for i in guesses:
-        print(i,end=" ")
+def guesstarget(target, guess):
+    if target == guess:
+        return 0 
         
-# ---------------------------------------------------------------------
-questions = {
-    "1/ Quel type d'ordinateur était le premier ordinateur portable ?":"C",
-    "2/ Quel est le plus grand réseau de médias sociaux au monde ?":"B",
-    "3/ Qui est considéré comme le fondateur de l’informatique moderne ?":"A",
-    "4/ En quelle année l’iPhone est-il sorti pour la première fois ? ":"A",
-    "5/ Un hibou vert est la mascotte de quelle application ?" :"C"
-}
-answers=[["A : Apple Macintosh","B : IBM PC","C : Osborne 1"],
-         ["A : Twitter","B : Facebook","C : Instagram"],
-         ["A : Alan Turing","B : Steve Jobs","C : Bill Gates"],
-         ["A : 2007","B : 2009","C : 2010"],
-         ["A : Spotify ","B : Tinder ","C : Duolingo"]]
+    elif target < guess:
+        return -1 
+    else:
+        return 1 
+    
+def gentarget():
+    import random
+    return random.randint(1, 20)
 
-new_game()
+print("Welcome to **Guess My Number!**\nI'm thinking of a number between 1 and 20.\nYou have to guess it in as 10 attempts possible.")
+target = gentarget()
+for i in  range(11):
+    print("Attempt %s" % (i+1))
+    guess = int(input("Enter your guess:"))
+    result = guesstarget(target,guess)
+    if  result == 0:
+        print("Congratulations! You guessed the number.")
+        break
+    elif  result == -1:
+        print("Your guess is too high. Try again.")
+    elif  result == 1:
+        print("Your guess is too low. Try again.")
+
+
